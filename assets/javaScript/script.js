43e290da5f23810f4e3a8e235a05a78683731c3e
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-10-04 00:15:18 -0400

    added scripting, working on looping array

---
+++ assets/javaScript/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    //watching save buttonc licks
+ $(".saveBtn").on("click", function(){
+    //get nearby values
+    var value = $(this).siblings("description").val();
+    var time =  $(this).parent().attr("id");
+
+    //local storage
+    localStorage.setItem(time,value);
+});
+
+function hourUpdater() {
+
+//current hours  numbers
+var currentHour = moment().hours();
+
+//time blocks looped in
+$(".time-block").each(function() {
+    var blockHour   = parseInt($(this).attr("id").split("-")[1]);
+
+
+    //verifying time moving
+    if (blockHour < currentHour){
+        $(this).addClass("past");
+    }
+    else if (blockHour === currentHour){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        }
+    });
+}
+
+hourUpdater();
+
+//interval for time to update
+var interval = setInterval( hourUpdater, 13000);
+
+
+
+//load in save data loop
+
+
+
+
+});
